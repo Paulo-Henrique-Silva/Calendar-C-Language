568c1f90014293e63ed9302784b2f56200b42e8f
Paulo Henrique Silva <pshenrique2015@gmail.com>
2021-12-16 16:38:31 -0300

    Simple Calendar Output

--- calendar.c
+++ calendar.c
@@ -5,7 +5,16 @@
 #include <conio.h>
 
 int main()
-{
-    printf("Calendar!");
+{ 
+    printf("\n\t\t\t\t      DEC - 2021");
+    printf("\n\t\t\t\t--------------------");
+    printf("\n\t\t\t\t S  M  T  W  T  F  S");
+    printf("\n\t\t\t\t       1  2  3  4  5");
+    printf("\n\t\t\t\t 6  7  8  9 10 11 12");
+    printf("\n\t\t\t\t13 14 15 16 17 18 19");
+    printf("\n\t\t\t\t20 21 22 23 24 25 26");
+    printf("\n\t\t\t\t27 28 29 30 31      ");
+
+    getch(); //stops before closes it
     return 0;
 }
